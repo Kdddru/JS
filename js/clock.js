45e8a227b9401fd6465c1ec1fe9aa39d589c132f
kdddru <dhk9817@gmail.com>
2023-02-20 23:23:00 +0900

    0220

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,11 @@ const clock = document.querySelector("#clock");
 
 function getClock(){
     const date = new Date();
-    console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+    const hour = String(date.getHours()).padStart(2, "0");
+    const minutes= String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerHTML = `${hour}:${minutes}:${seconds}`;
 }
 
-setInterval(getClock,5000);
+getClock();
+setInterval(getClock,1000);
